9d5f7cd223cf8c0fae95d75aab37ac6b8d74e0c5
Nidhi Musale <musan01@pfw.edu>
2025-11-19 13:31:31 -0500

    Complete Frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/mastoride.css";
+import "./styles/style1.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
